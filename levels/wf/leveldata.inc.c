59872d10f72d23276c3c17da5a975c68a7996e1d
MelroyOldenburger <melroy777g@gmail.com>
2022-08-22 03:54:02 +0200

    Jump into the stump in PAIN for janky slide

--- levels/wf/leveldata.inc.c
+++ levels/wf/leveldata.inc.c
@@ -1,4 +1,8 @@
 #include "levels/wf/area_1/collision.inc.c"
 #include "levels/wf/area_1/macro.inc.c"
 #include "levels/wf/area_1/spline.inc.c"
+#include "levels/wf/area_2/collision.inc.c"
+#include "levels/wf/area_2/room.inc.c"
+#include "levels/wf/area_2/macro.inc.c"
+#include "levels/wf/area_2/spline.inc.c"
 #include "levels/wf/model.inc.c"
