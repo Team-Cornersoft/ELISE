b829a44b98e164a919629efafbc1a2537f5bcdc3
MelroyOldenburger <melroy777g@gmail.com>
2022-08-26 00:09:47 +0200

    HOME up to area 6 done

--- levels/ccm/texscroll.inc.c
+++ levels/ccm/texscroll.inc.c
@@ -134,6 +134,7 @@ void scroll_ccm_dl_B_bone_and_poo_mesh_layer_4_vtx_0() {
 	int i = 0;
 	int count = 82;
 	int width = 128 * 0x20;
+	int height = 16 * 0x20;
 
 	static int currentX = 0;
 	int deltaX;
@@ -155,6 +156,7 @@ void scroll_ccm_dl_poopsdlide_001_mesh_layer_4_vtx_0() {
 	int i = 0;
 	int count = 82;
 	int width = 128 * 0x20;
+	int height = 16 * 0x20;
 
 	static int currentX = 0;
 	int deltaX;
@@ -176,6 +178,7 @@ void scroll_ccm_dl_poopsdlide_002_mesh_layer_4_vtx_0() {
 	int i = 0;
 	int count = 82;
 	int width = 128 * 0x20;
+	int height = 16 * 0x20;
 
 	static int currentX = 0;
 	int deltaX;
@@ -193,6 +196,64 @@ void scroll_ccm_dl_poopsdlide_002_mesh_layer_4_vtx_0() {
 	currentX += deltaX;
 }
 
+void scroll_sts_mat_ccm_dl_Water_A_layer5() {
+	Gfx *mat = segmented_to_virtual(mat_ccm_dl_Water_A_layer5);
+	shift_s_down(mat, 15, PACK_TILESIZE(0, 1));
+	shift_t_down(mat, 15, PACK_TILESIZE(0, 1));
+	shift_s(mat, 23, PACK_TILESIZE(0, 1));
+};
+
+void scroll_sts_mat_ccm_dl_Water_B_layer5() {
+	Gfx *mat = segmented_to_virtual(mat_ccm_dl_Water_B_layer5);
+	shift_s_down(mat, 15, PACK_TILESIZE(0, 1));
+	shift_s(mat, 23, PACK_TILESIZE(0, 1));
+	shift_t_down(mat, 23, PACK_TILESIZE(0, 1));
+};
+
+void scroll_sts_mat_ccm_dl_dino3() {
+	static int intervalTex0 = 2;
+	static int curInterval0 = 2;
+	Gfx *mat = segmented_to_virtual(mat_ccm_dl_dino3);
+
+	if (--curInterval0 <= 0) {
+		shift_s(mat, 19, PACK_TILESIZE(0, 128));
+		curInterval0 = intervalTex0;
+	}
+};
+
+void scroll_sts_mat_ccm_dl_dino4() {
+	static int intervalTex0 = 2;
+	static int curInterval0 = 2;
+	Gfx *mat = segmented_to_virtual(mat_ccm_dl_dino4);
+
+	if (--curInterval0 <= 0) {
+		shift_s(mat, 19, PACK_TILESIZE(0, 128));
+		curInterval0 = intervalTex0;
+	}
+};
+
+void scroll_sts_mat_ccm_dl_dino2() {
+	static int intervalTex0 = 2;
+	static int curInterval0 = 2;
+	Gfx *mat = segmented_to_virtual(mat_ccm_dl_dino2);
+
+	if (--curInterval0 <= 0) {
+		shift_s(mat, 19, PACK_TILESIZE(0, 128));
+		curInterval0 = intervalTex0;
+	}
+};
+
+void scroll_sts_mat_ccm_dl_dino1() {
+	static int intervalTex0 = 2;
+	static int curInterval0 = 2;
+	Gfx *mat = segmented_to_virtual(mat_ccm_dl_dino1);
+
+	if (--curInterval0 <= 0) {
+		shift_s(mat, 19, PACK_TILESIZE(0, 128));
+		curInterval0 = intervalTex0;
+	}
+};
+
 void scroll_ccm() {
 	scroll_sts_mat_ccm_dl_bird3();
 	scroll_sts_mat_ccm_dl_bird4();
@@ -209,4 +270,10 @@ void scroll_ccm() {
 	scroll_ccm_dl_B_bone_and_poo_mesh_layer_4_vtx_0();
 	scroll_ccm_dl_poopsdlide_001_mesh_layer_4_vtx_0();
 	scroll_ccm_dl_poopsdlide_002_mesh_layer_4_vtx_0();
+	scroll_sts_mat_ccm_dl_Water_A_layer5();
+	scroll_sts_mat_ccm_dl_Water_B_layer5();
+	scroll_sts_mat_ccm_dl_dino3();
+	scroll_sts_mat_ccm_dl_dino4();
+	scroll_sts_mat_ccm_dl_dino2();
+	scroll_sts_mat_ccm_dl_dino1();
 }
