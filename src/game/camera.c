bb2dd9652c9f9136844ec3252219a05236ec1a81
MelroyOldenburger <melroy777g@gmail.com>
2022-08-27 00:23:44 +0200

    Boss battle arena

--- src/game/camera.c
+++ src/game/camera.c
@@ -5966,6 +5966,9 @@ struct CameraTrigger sCamWF[] = {
 struct CameraTrigger sCamJRB[] = {
 	NULL_TRIGGER
 };
+struct CameraTrigger sCamBowser_1[] = {
+	NULL_TRIGGER
+};
 struct CameraTrigger *sCameraTriggers[LEVEL_COUNT + 1] = {
     NULL,
     #include "levels/level_defines.h"
