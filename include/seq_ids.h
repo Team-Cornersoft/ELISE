fef50e54676692e532e9965856eaefc62e26b688
gheskett <gheskett@gmail.com>
2022-08-27 22:30:41 -0400

    Revert "Tested song, it sounds good, though a bit quieter than I thought"
    This reverts commit 388c526fd115f560df5cd0b59a01bcf5bfad176c.

--- include/seq_ids.h
+++ include/seq_ids.h
@@ -46,7 +46,6 @@ enum SeqId {
     SEQ_MENU_FILE_SELECT,             // 0x21
     SEQ_EVENT_CUTSCENE_LAKITU,        // 0x22 (not in JP)
     SEQ_LEVEL_LOSS,                   // 0x23
-    SEQ_LEVEL_HOME_TOGETHER,          // 0x24
     SEQ_COUNT
 };
 
