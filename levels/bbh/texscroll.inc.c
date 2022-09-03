3c87ac344554c309d1ec5a56ba96a82ab51230e5
MelroyOldenburger <melroy777g@gmail.com>
2022-09-03 22:56:55 +0200

    BLOOM update

--- levels/bbh/texscroll.inc.c
+++ levels/bbh/texscroll.inc.c
@@ -1,3 +1,25 @@
+void scroll_bbh_dl_B_dress_mesh_layer_5_vtx_1() {
+	int i = 0;
+	int count = 52;
+	int width = 128 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(bbh_dl_B_dress_mesh_layer_5_vtx_1);
+
+	deltaX = (int)(0.20000000298023224 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
 void scroll_bbh_dl_B_eyes_mesh_layer_1_vtx_0() {
 	int i = 0;
 	int count = 112;
@@ -9,6 +31,94 @@ void scroll_bbh_dl_B_eyes_mesh_layer_1_vtx_0() {
 	Vtx *vertices = segmented_to_virtual(bbh_dl_B_eyes_mesh_layer_1_vtx_0);
 
 	deltaX = (int)(-0.20000000298023224 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
+void scroll_bbh_dl_C_Flower_guide_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 18;
+	int width = 128 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(bbh_dl_C_Flower_guide_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.20000000298023224 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
+void scroll_bbh_dl_C_Flower_guide_001_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 18;
+	int width = 128 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(bbh_dl_C_Flower_guide_001_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.20000000298023224 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
+void scroll_bbh_dl_C_Flower_guide_002_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 18;
+	int width = 128 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(bbh_dl_C_Flower_guide_002_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.20000000298023224 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
+void scroll_bbh_dl_C_Flower_guide_003_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 18;
+	int width = 128 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(bbh_dl_C_Flower_guide_003_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.20000000298023224 * 0x20) % width;
 
 	if (absi(currentX) > width) {
 		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
@@ -34,17 +144,17 @@ void scroll_sts_mat_bbh_dl_Sparkles_B_layer5() {
 	shift_t_down(mat, 23, PACK_TILESIZE(0, 1));
 };
 
-void scroll_bbh_dl_E_dress_mesh_layer_5_vtx_1() {
+void scroll_bbh_dl_F_Sun_mesh_layer_5_vtx_1() {
 	int i = 0;
-	int count = 52;
-	int width = 128 * 0x20;
+	int count = 86;
+	int width = 32 * 0x20;
 	int height = 32 * 0x20;
 
 	static int currentX = 0;
 	int deltaX;
-	Vtx *vertices = segmented_to_virtual(bbh_dl_E_dress_mesh_layer_5_vtx_1);
+	Vtx *vertices = segmented_to_virtual(bbh_dl_F_Sun_mesh_layer_5_vtx_1);
 
-	deltaX = (int)(0.20000000298023224 * 0x20) % width;
+	deltaX = (int)(-0.20000000298023224 * 0x20) % width;
 
 	if (absi(currentX) > width) {
 		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
@@ -57,8 +167,13 @@ void scroll_bbh_dl_E_dress_mesh_layer_5_vtx_1() {
 }
 
 void scroll_bbh() {
+	scroll_bbh_dl_B_dress_mesh_layer_5_vtx_1();
 	scroll_bbh_dl_B_eyes_mesh_layer_1_vtx_0();
+	scroll_bbh_dl_C_Flower_guide_mesh_layer_5_vtx_0();
+	scroll_bbh_dl_C_Flower_guide_001_mesh_layer_5_vtx_0();
+	scroll_bbh_dl_C_Flower_guide_002_mesh_layer_5_vtx_0();
+	scroll_bbh_dl_C_Flower_guide_003_mesh_layer_5_vtx_0();
 	scroll_sts_mat_bbh_dl_Sparkles_A_layer5();
 	scroll_sts_mat_bbh_dl_Sparkles_B_layer5();
-	scroll_bbh_dl_E_dress_mesh_layer_5_vtx_1();
+	scroll_bbh_dl_F_Sun_mesh_layer_5_vtx_1();
 }
--- levels/bbh/texscroll.inc.h
+++ levels/bbh/texscroll.inc.h
@@ -1,5 +1,10 @@
+extern void scroll_bbh_dl_B_dress_mesh_layer_5_vtx_1();
 extern void scroll_bbh_dl_B_eyes_mesh_layer_1_vtx_0();
+extern void scroll_bbh_dl_C_Flower_guide_mesh_layer_5_vtx_0();
+extern void scroll_bbh_dl_C_Flower_guide_001_mesh_layer_5_vtx_0();
+extern void scroll_bbh_dl_C_Flower_guide_002_mesh_layer_5_vtx_0();
+extern void scroll_bbh_dl_C_Flower_guide_003_mesh_layer_5_vtx_0();
 extern void scroll_sts_mat_bbh_dl_Sparkles_A_layer5();
 extern void scroll_sts_mat_bbh_dl_Sparkles_B_layer5();
-extern void scroll_bbh_dl_E_dress_mesh_layer_5_vtx_1();
+extern void scroll_bbh_dl_F_Sun_mesh_layer_5_vtx_1();
 extern void scroll_bbh();
