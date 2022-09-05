7a671cc35010d1fc026b848626b8f465d5f2faca
MelroyOldenburger <melroy777g@gmail.com>
2022-09-05 23:07:29 +0200

    Secrets with models now

--- levels/ttm/texscroll.inc.c
+++ levels/ttm/texscroll.inc.c
@@ -4,27 +4,20 @@ void scroll_ttm_dl_C_UFO_mesh_layer_1_vtx_2() {
 	int width = 32 * 0x20;
 	int height = 32 * 0x20;
 
-	static int currentX = 0;
-	int deltaX;
 	static int currentY = 0;
 	int deltaY;
 	Vtx *vertices = segmented_to_virtual(ttm_dl_C_UFO_mesh_layer_1_vtx_2);
 
-	deltaX = (int)(2.0 * 0x20) % width;
 	deltaY = (int)(2.0 * 0x20) % height;
 
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
-	}
 	if (absi(currentY) > height) {
 		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
 	}
 
 	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
 		vertices[i].n.tc[1] += deltaY;
 	}
-	currentX += deltaX;	currentY += deltaY;
+	currentY += deltaY;
 }
 
 void scroll_sts_mat_ttm_dl_Sparkles_A_layer5() {
