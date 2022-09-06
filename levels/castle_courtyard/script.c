a8b00ec5ac633d737ca107a94fe7bf232d9e7d9e
gheskett <gheskett@gmail.com>
2022-09-06 19:34:33 -0400

    Hopefully bypass some fast64 shenanigans with title screen

--- levels/castle_courtyard/script.c
+++ levels/castle_courtyard/script.c
@@ -10,65 +10,20 @@
 #include "game/level_update.h"
 
 #include "levels/scripts.h"
-#include "levels/intro/header.h"
-#include "game/area.h"
-#include "menu/title_screen.h"
-#include "game/texscroll.h"
 
 #include "actors/common1.h"
 
 /* Fast64 begin persistent block [includes] */
+#include "levels/intro/header.h"
+#include "game/area.h"
+#include "menu/title_screen.h"
+#include "game/texscroll.h"
 /* Fast64 end persistent block [includes] */
 
 #include "make_const_nonconst.h"
 #include "levels/castle_courtyard/header.h"
 
 /* Fast64 begin persistent block [scripts] */
-/* Fast64 end persistent block [scripts] */
-
-const LevelScript level_castle_courtyard_entry[] = {
-	INIT_LEVEL(),
-	LOAD_YAY0(0x07, _castle_courtyard_segment_7SegmentRomStart, _castle_courtyard_segment_7SegmentRomEnd), 
-	LOAD_YAY0(0x0A, _water_skybox_yay0SegmentRomStart, _water_skybox_yay0SegmentRomEnd), 
-	LOAD_YAY0_TEXTURE(0x09, _outside_yay0SegmentRomStart, _outside_yay0SegmentRomEnd), 
-	LOAD_YAY0(0x05, _group9_yay0SegmentRomStart, _group9_yay0SegmentRomEnd), 
-	LOAD_RAW(0x0C, _group9_geoSegmentRomStart, _group9_geoSegmentRomEnd), 
-	LOAD_YAY0(0x08, _common0_yay0SegmentRomStart, _common0_yay0SegmentRomEnd), 
-	LOAD_RAW(0x0F, _common0_geoSegmentRomStart, _common0_geoSegmentRomEnd), 
-	ALLOC_LEVEL_POOL(),
-	MARIO(MODEL_MARIO, 0x00000001, bhvMario), 
-	JUMP_LINK(script_func_global_1), 
-	JUMP_LINK(script_func_global_10), 
-	LOAD_MODEL_FROM_GEO(MODEL_COURTYARD_SPIKY_TREE, spiky_tree_geo), 
-	LOAD_MODEL_FROM_GEO(MODEL_COURTYARD_WOODEN_DOOR, wooden_door_geo), 
-	LOAD_MODEL_FROM_GEO(MODEL_LEVEL_GEOMETRY_03, castle_courtyard_geo_000200), 
-
-	/* Fast64 begin persistent block [level commands] */
-	/* Fast64 end persistent block [level commands] */
-
-	AREA(1, castle_courtyard_area_1),
-		WARP_NODE(0x0A, LEVEL_WF, 0x01, 0x0A, WARP_NO_CHECKPOINT),
-		WARP_NODE(0xF0, LEVEL_BOB, 0x01, 0x0A, WARP_NO_CHECKPOINT),
-		WARP_NODE(0xF1, LEVEL_BOB, 0x01, 0x0A, WARP_NO_CHECKPOINT),
-		OBJECT(MODEL_NONE, -2400, -800, 0, 0, -180, 0, 0x000A0000, bhvAirborneDeathWarp),
-		MARIO_POS(0x01, -180, -2400, -800, 0),
-		TERRAIN(castle_courtyard_area_1_collision),
-		MACRO_OBJECTS(castle_courtyard_area_1_macro_objs),
-		SET_BACKGROUND_MUSIC(0x00, SEQ_LEVEL_GRASS),
-		TERRAIN_TYPE(TERRAIN_STONE),
-		/* Fast64 begin persistent block [area commands] */
-		/* Fast64 end persistent block [area commands] */
-	END_AREA(),
-
-	FREE_LEVEL_POOL(),
-	MARIO_POS(0x01, -180, -2400, -800, 0),
-	CALL(0, lvl_init_or_update),
-	CALL_LOOP(1, lvl_init_or_update),
-	CLEAR_LEVEL(),
-	SLEEP_BEFORE_EXIT(1),
-	EXIT(),
-};
-
 const LevelScript level_ccy_title_screen[] = {
     INIT_LEVEL(),
 	LOAD_YAY0(0x07, _castle_courtyard_segment_7SegmentRomStart, _castle_courtyard_segment_7SegmentRomEnd), 
@@ -116,4 +71,48 @@ const LevelScript level_ccy_title_screen[] = {
     CLEAR_LEVEL(),
     SLEEP(/*frames*/ 2),
     EXIT_AND_EXECUTE_WITH_CODE(/*seg*/ SEGMENT_MENU_INTRO, _introSegmentRomStart, _introSegmentRomEnd, level_intro_mario_head_regular, _introSegmentBssStart, _introSegmentBssEnd),
+};
+/* Fast64 end persistent block [scripts] */
+
+const LevelScript level_castle_courtyard_entry[] = {
+	INIT_LEVEL(),
+	LOAD_YAY0(0x07, _castle_courtyard_segment_7SegmentRomStart, _castle_courtyard_segment_7SegmentRomEnd), 
+	LOAD_YAY0(0x0A, _water_skybox_yay0SegmentRomStart, _water_skybox_yay0SegmentRomEnd), 
+	LOAD_YAY0_TEXTURE(0x09, _outside_yay0SegmentRomStart, _outside_yay0SegmentRomEnd), 
+	LOAD_YAY0(0x05, _group9_yay0SegmentRomStart, _group9_yay0SegmentRomEnd), 
+	LOAD_RAW(0x0C, _group9_geoSegmentRomStart, _group9_geoSegmentRomEnd), 
+	LOAD_YAY0(0x08, _common0_yay0SegmentRomStart, _common0_yay0SegmentRomEnd), 
+	LOAD_RAW(0x0F, _common0_geoSegmentRomStart, _common0_geoSegmentRomEnd), 
+	ALLOC_LEVEL_POOL(),
+	MARIO(MODEL_MARIO, 0x00000001, bhvMario), 
+	JUMP_LINK(script_func_global_1), 
+	JUMP_LINK(script_func_global_10), 
+	LOAD_MODEL_FROM_GEO(MODEL_COURTYARD_SPIKY_TREE, spiky_tree_geo), 
+	LOAD_MODEL_FROM_GEO(MODEL_COURTYARD_WOODEN_DOOR, wooden_door_geo), 
+	LOAD_MODEL_FROM_GEO(MODEL_LEVEL_GEOMETRY_03, castle_courtyard_geo_000200), 
+
+	/* Fast64 begin persistent block [level commands] */
+	/* Fast64 end persistent block [level commands] */
+
+	AREA(1, castle_courtyard_area_1),
+		WARP_NODE(0x0A, LEVEL_WF, 0x01, 0x0A, WARP_NO_CHECKPOINT),
+		WARP_NODE(0xF0, LEVEL_BOB, 0x01, 0x0A, WARP_NO_CHECKPOINT),
+		WARP_NODE(0xF1, LEVEL_BOB, 0x01, 0x0A, WARP_NO_CHECKPOINT),
+		OBJECT(MODEL_NONE, -2400, -800, 0, 0, -180, 0, 0x000A0000, bhvAirborneDeathWarp),
+		MARIO_POS(0x01, -180, -2400, -800, 0),
+		TERRAIN(castle_courtyard_area_1_collision),
+		MACRO_OBJECTS(castle_courtyard_area_1_macro_objs),
+		SET_BACKGROUND_MUSIC(0x00, SEQ_LEVEL_GRASS),
+		TERRAIN_TYPE(TERRAIN_STONE),
+		/* Fast64 begin persistent block [area commands] */
+		/* Fast64 end persistent block [area commands] */
+	END_AREA(),
+
+	FREE_LEVEL_POOL(),
+	MARIO_POS(0x01, -180, -2400, -800, 0),
+	CALL(0, lvl_init_or_update),
+	CALL_LOOP(1, lvl_init_or_update),
+	CLEAR_LEVEL(),
+	SLEEP_BEFORE_EXIT(1),
+	EXIT(),
 };
