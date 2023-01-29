ffd56408ec18bbe83d28734fc1f9d7e394537e3b
Gregory Heskett <gheskett@gmail.com>
2023-01-29 15:04:39 -0500

    Start samples at full velocity when using envelopes that start with a delay of 1
    This can help eliminate sample artifacting that happens during a transition between a sudden fade-in into normal volume. It's not completely eliminated due to some processing weirdness, but it's a massive improvement.

--- src/audio/playback.c
+++ src/audio/playback.c
@@ -580,7 +580,7 @@ void process_notes(void) {
                 }
             }
 
-            adsr_update(&note->adsr);
+            adsr_update(note);
             note_vibrato_update(note);
             attributes = &note->attributes;
             if (note->priority == NOTE_PRIORITY_STOPPING) {
@@ -1467,6 +1467,7 @@ void note_init_all(void) {
 #else
         note->reverbVol = 0;
         note->usesHeadsetPanEffects = FALSE;
+        note->initFullVelocity = FALSE;
         note->sampleCount = 0;
         note->instOrWave = 0;
         note->targetVolLeft = 0;
